641c98da8faf06ebc25b0e57f35e7ca4cf4df573
zhanpenghe <zhanpenh@usc.edu>
2019-06-28 18:26:33 -0700

    Add baby mode experiments

---
+++ corl/baby/baby_push.py
@@ -0,0 +1,103 @@
+from maml_zoo.baselines.linear_baseline import LinearFeatureBaseline
+from maml_zoo.meta_algos.trpo_maml import TRPOMAML
+from maml_zoo.meta_trainer import Trainer
+import argparse
+
+from maml_zoo.samplers.maml_sampler import MAMLSampler
+from maml_zoo.samplers.maml_sample_processor import MAMLSampleProcessor
+from maml_zoo.policies.meta_gaussian_mlp_policy import MetaGaussianMLPPolicy
+import os
+from maml_zoo.logger import logger
+import json
+import numpy as np
+
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_reach_push_pick_place_6dof import SawyerReachPushPickPlace6DOFEnv
+from baby_wrapper import BabyModeWrapper
+maml_zoo_path = '/'.join(os.path.realpath(os.path.dirname(__file__)).split('/')[:-1])
+
+N_TASKS = 50
+TASKNAME = 'push'
+
+
+def main(config):
+
+    goal_low = np.array((-0.1, 0.8, 0.2))
+    goal_high = np.array((0.1, 0.9, 0.2))
+
+    goals = np.random.uniform(low=goal_low, high=goal_high, size=(N_TASKS, len(goal_low))).tolist()
+    print(goals)
+
+    tasks =[
+        {'goal': np.array(g), 'obj_init_pos':np.array([0, 0.6, 0.02]), 'obj_init_angle': 0.3, 'type':'push'}
+        for i, g in enumerate(goals)
+    ]
+
+    baseline = LinearFeatureBaseline()
+    env = BabyModeWrapper(SawyerReachPushPickPlace6DOFEnv(
+        random_init=False,
+        multitask=False,
+        obs_type='plain',
+        if_render=False,
+        tasks=tasks,
+    ))
+
+    policy = MetaGaussianMLPPolicy(
+            name="meta-policy",
+            obs_dim=np.prod(env.observation_space.shape),
+            action_dim=np.prod(env.action_space.shape),
+            meta_batch_size=config['meta_batch_size'],
+            hidden_sizes=config['hidden_sizes'],
+        )
+
+    sampler = MAMLSampler(
+        env=env,
+        policy=policy,
+        rollouts_per_meta_task=config['rollouts_per_meta_task'],  # This batch_size is confusing
+        meta_batch_size=config['meta_batch_size'],
+        max_path_length=config['max_path_length'],
+        parallel=config['parallel'],
+        envs_per_task=config['envs_per_task']
+    )
+
+    sample_processor = MAMLSampleProcessor(
+        baseline=baseline,
+        discount=config['discount'],
+        gae_lambda=config['gae_lambda'],
+        normalize_adv=config['normalize_adv'],
+        positive_adv=config['positive_adv'],
+    )
+
+    algo = TRPOMAML(
+        policy=policy,
+        step_size=config['step_size'],
+        inner_type=config['inner_type'],
+        meta_batch_size=config['meta_batch_size'],
+        num_inner_grad_steps=config['num_inner_grad_steps'],
+        inner_lr=config['inner_lr']
+    )
+
+    trainer = Trainer(
+        algo=algo,
+        policy=policy,
+        env=env,
+        sampler=sampler,
+        sample_processor=sample_processor,
+        n_itr=config['n_itr'],
+        num_inner_grad_steps=config['num_inner_grad_steps'],  # This is repeated in MAMLPPO, it's confusing
+    )
+    trainer.train()
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Play a pickled policy.')
+    parser.add_argument('variant_index', metavar='variant_index', type=int,
+                    help='The index of variants to use for experiment')
+    args = parser.parse_args()
+
+    rand_num = np.random.uniform()
+    idx = args.variant_index
+    logger.configure(dir=maml_zoo_path + '/data/trpo/test_{}_{}_{}'.format(TASKNAME, idx, rand_num), format_strs=['stdout', 'log', 'csv'],
+                     snapshot_mode='last_gap')
+    config = json.load(open("./corl/configs/baby_mode_config{}.json".format(idx), 'r'))
+    json.dump(config, open(maml_zoo_path + '/data/trpo/test_{}_{}_{}/params.json'.format(TASKNAME, idx, rand_num), 'w'))
+    main(config)
---
+++ corl/baby/baby_wrapper.py
@@ -0,0 +1,26 @@
+import gym
+from maml_zoo.utils import Serializable
+
+
+class BabyModeWrapper(gym.Wrapper, Serializable):
+
+    def __init__(self, env):
+        Serializable.quick_init(self, locals())
+        super().__init__(env)
+
+    '''
+    MAML sampler api
+    
+    In baby mode, tasks will be reset in the environment reset
+    function so we ignore task sampling/setting
+
+    Tasks should be sampled before construnction.
+    '''
+    def sample_tasks(self, meta_batch_size):
+        return [None] * meta_batch_size
+    
+    def set_task(self, task):
+        pass
+
+    def log_diagnostics(self, paths, prefix):
+        pass
