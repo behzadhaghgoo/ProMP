59c66716718d2ca57a3e833cfca560a99ec68c01
CatherineSue <8605658+CatherineSue@users.noreply.github.com>
2019-07-03 13:45:54 -0700

    Add rollout

---
+++ corl/baby/baby_push_rollout.py
@@ -0,0 +1,148 @@
+import argparse
+import os
+
+import joblib
+import json
+import numpy as np
+import tensorflow as tf
+
+from maml_zoo.baselines.linear_baseline import LinearFeatureBaseline
+from maml_zoo.logger import logger
+from maml_zoo.meta_algos.trpo_maml import TRPOMAML
+from maml_zoo.meta_trainer import Trainer
+from maml_zoo.policies.meta_gaussian_mlp_policy import MetaGaussianMLPPolicy
+from maml_zoo.samplers.maml_sampler import MAMLSampler
+from maml_zoo.samplers.maml_sample_processor import MAMLSampleProcessor
+
+from multiworld.envs.mujoco.sawyer_xyz.sawyer_reach_push_pick_place_6dof import SawyerReachPushPickPlace6DOFEnv
+from baby_wrapper import BabyModeWrapper
+
+maml_zoo_path = '/'.join(os.path.realpath(os.path.dirname(__file__)).split('/')[:-1])
+
+N_TASKS = 1
+TASKNAME = 'push'
+
+
+def rollout(experiment):
+    env = experiment['env'].env
+    tasks = env.tasks
+
+    policy = experiment['policy']
+    o = env.reset()
+
+    n_timesteps = 150
+
+    for i in range(n_timesteps):
+        env.render()
+        a, _ = policy.get_action(o)
+        o, _, d, _ = env.step(a)
+        if d:
+            break
+
+
+def maml_test(experiment, config, sess, start_itr):
+    pickled_env = experiment['env'].env
+    pickled_tasks = pickled_env.tasks
+
+    goal_low = np.array((-0.1, 0.8, 0.02))
+    goal_high = np.array((0.1, 0.9, 0.02))
+
+    goals = np.random.uniform(low=goal_low, high=goal_high, size=(N_TASKS, len(goal_low))).tolist()
+    print(goals)
+
+    tasks =[
+        {'goal': np.array(g), 'obj_init_pos':np.array([0, 0.6, 0.02]), 'obj_init_angle': 0.3, 'type':'push'}
+        for i, g in enumerate(goals)
+    ]
+
+    tasks = pickled_tasks[0:20]
+
+    baseline = LinearFeatureBaseline()
+    env = BabyModeWrapper(SawyerReachPushPickPlace6DOFEnv(
+        random_init=False,
+        multitask=False,
+        obs_type='plain',
+        if_render=False,
+        tasks=tasks,
+    ))
+
+    policy = experiment['policy']
+
+    sampler = MAMLSampler(
+        env=env,
+        policy=policy,
+        rollouts_per_meta_task=config['rollouts_per_meta_task'],  # This batch_size is confusing
+        meta_batch_size=config['meta_batch_size'],
+        max_path_length=config['max_path_length'],
+        parallel=config['parallel'],
+        envs_per_task=config['envs_per_task']
+    )
+
+    sample_processor = MAMLSampleProcessor(
+        baseline=baseline,
+        discount=config['discount'],
+        gae_lambda=config['gae_lambda'],
+        normalize_adv=config['normalize_adv'],
+        positive_adv=config['positive_adv'],
+    )
+
+    algo = TRPOMAML(
+        policy=policy,
+        step_size=config['step_size'],
+        inner_type=config['inner_type'],
+        meta_batch_size=config['meta_batch_size'],
+        num_inner_grad_steps=config['num_inner_grad_steps'],
+        inner_lr=config['inner_lr']
+    )
+
+    trainer = Trainer(
+        algo=algo,
+        policy=policy,
+        env=env,
+        sampler=sampler,
+        sample_processor=sample_processor,
+        n_itr=config['n_itr'],
+        num_inner_grad_steps=config['num_inner_grad_steps'],  # This is repeated in MAMLPPO, it's confusing
+        sess=sess,
+        start_itr=start_itr,
+    )
+
+    trainer.train(test_time=True)
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Play a pickled policy.')
+    parser.add_argument('variant_index', metavar='variant_index', type=int,
+                    help='The index of variants to use for experiment')
+    parser.add_argument('--pkl', metavar='pkl', type=str,
+                    help='The path of the pkl file', 
+                    default=None, required=False)
+    parser.add_argument('--config', metavar='config', type=str,
+                    help='The path to the config file', 
+                    default=None, required=False)
+    parser.add_argument('--itr', metavar='itr', type=int,
+                    help='The start itr of the resuming experiment', 
+                    default=0, required=False)
+    args = parser.parse_args()
+
+    rand_num = np.random.uniform()
+    idx = args.variant_index
+    pkl = args.pkl
+    config = args.config
+    itr = args.itr
+
+    if not config:
+        config = './corl/configs/baby_mode_config{}.json'.format(idx)
+    
+    if pkl:
+        with tf.Session() as sess:
+            with open(pkl, 'rb') as file:
+                experiment = joblib.load(file)
+            # logger.configure(dir=maml_zoo_path + '/data/maml_test/test_{}_{}_{}'.format(TASKNAME, idx, rand_num), format_strs=['stdout', 'log', 'csv'],
+            #          snapshot_mode='all',)
+            # config = json.load(open(config, 'r'))
+            # json.dump(config, open(maml_zoo_path + '/data/maml_test/test_{}_{}_{}/params.json'.format(TASKNAME, idx, rand_num), 'w'))
+            # maml_test(experiment, config, sess, itr)
+                rollout(experiment)
+    else:
+        print('Please provide a pkl file')
