2bf63a881d52e45f21e204cf984c9acdd6b0ca1e
zhanpenghe <zhanpenh@usc.edu>
2019-07-06 23:15:12 -0700

    Add all test scripts

---
+++ corl/baby/maml_pickplace_test.sh
@@ -0,0 +1,6 @@
+echo "$1"
+
+for i in $(seq 0 $2)
+do
+    PYTHONPATH=/root/code/ProMP:/root/code/ProMP/metaworld:/root/code/ProMP/corl/baby python ./maml_pickplace_test.py 0 --pkl $1/itr_$i.pkl
+done
