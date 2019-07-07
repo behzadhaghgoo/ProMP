629cd5b9dc4167a7410836082007c36d779ebd5b
zhanpenghe <zhanpenh@usc.edu>
2019-07-06 21:21:01 -0700

    Fix python script name

--- corl/medium_test_train_envs.sh
+++ corl/medium_test_train_envs.sh
@@ -1,5 +1,5 @@
 echo $1
 for i in $(seq $2 5 $3)
 do
-    PYTHONPATH=/root/code/ProMP:/root/code/ProMP/metaworld:/root/code/ProMP/corl/baby python /root/code/ProMP/corl/medium_test_train_env.py 0 --pkl $1/itr_$i.pkl
+    PYTHONPATH=/root/code/ProMP:/root/code/ProMP/metaworld:/root/code/ProMP/corl/baby python /root/code/ProMP/corl/medium_test_train_envs.py 0 --pkl $1/itr_$i.pkl
 done
