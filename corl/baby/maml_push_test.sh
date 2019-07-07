a4e013b337d435debaf99c020313b9ae71d338d3
zhanpenghe <zhanpenh@usc.edu>
2019-07-06 23:28:17 -0700

    Change variant idx to 1 in shell scripts

--- corl/baby/maml_push_test.sh
+++ corl/baby/maml_push_test.sh
@@ -2,5 +2,5 @@ echo "$1"
 
 for i in $(seq 0 $2)
 do
-    PYTHONPATH=/root/code/ProMP:/root/code/ProMP/metaworld:/root/code/ProMP/corl/baby python ./maml_push_test.py 0 --pkl $1/itr_$i.pkl
+    PYTHONPATH=/root/code/ProMP:/root/code/ProMP/metaworld:/root/code/ProMP/corl/baby python ./maml_push_test.py 1 --pkl $1/itr_$i.pkl
 done
