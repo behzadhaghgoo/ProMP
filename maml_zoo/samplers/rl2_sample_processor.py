Fix AverageSuccessRate metric calculation

maml_zoo/samplers/rl2_sample_processor.py
@@ -49,7 +49,7 @@ class RL2SampleProcessor(SampleProcessor):
         # 8) log statistics if desired
         self._log_path_stats(all_paths, log=log, log_prefix=log_prefix)
 
-        average_success_rate = np.mean(np.stack([p['env_infos']['success'] for p in paths]))
+        average_success_rate = np.mean(np.stack([np.any(p['env_infos']['success'] == 1) for p in paths]))
         logger.logkv(log_prefix + 'AverageSuccessRate', average_success_rate)
 
         samples_data = dict(