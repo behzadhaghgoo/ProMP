5c05c8d916ad82f09c7fb82f404c7d133ab68c06
Zhanpeng He <zhanpenh@usc.edu>
2019-07-26 13:11:37 +0800

    Fix launcher

--- corl/rl2/med_train_two_tasks.py
+++ corl/rl2/med_train_two_tasks.py
@@ -19,7 +19,7 @@ from maml_zoo.policies.gaussian_rnn_policy import GaussianRNNPolicy
 from maml_zoo.logger import logger
 
 
-def main(experiment, config, sess, start_itr, all_params):
+def main(config):
     import collections
     from metaworld.envs.mujoco.sawyer_xyz.sawyer_reach_push_pick_place_6dof import SawyerReachPushPickPlace6DOFEnv
     from metaworld.envs.mujoco.sawyer_xyz.sawyer_dial_turn_6dof import SawyerDialTurn6DOFEnv
