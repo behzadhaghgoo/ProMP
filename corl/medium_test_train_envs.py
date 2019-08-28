1bacafdcd60f5aa4af112b8fda072b088fe63796
zhanpeng <zhanpeng.he@columbia.edu>
2019-08-28 09:04:22 +0000

    Configs tried for medium mode

--- corl/medium_test_train_envs.py
+++ corl/medium_test_train_envs.py
@@ -9,14 +9,13 @@ import tensorflow as tf
 from maml_zoo.baselines.linear_baseline import LinearFeatureBaseline
 from maml_zoo.logger import logger
 from maml_zoo.meta_algos.trpo_maml import TRPOMAML
-from maml_zoo.envs.multitask_env import MultiClassMultiTaskEnv
 from maml_zoo.meta_trainer import Trainer
 from maml_zoo.policies.meta_gaussian_mlp_policy import MetaGaussianMLPPolicy
 from maml_zoo.samplers.maml_sampler import MAMLSampler
 from maml_zoo.samplers.maml_sample_processor import MAMLSampleProcessor
 
-from multiworld.envs.mujoco.sawyer_xyz.sawyer_reach_push_pick_place_6dof import SawyerReachPushPickPlace6DOFEnv
-from baby_wrapper import BabyModeWrapper
+from metaworld.envs.mujoco.multitask_env import MultiClassMultiTaskEnv
+
 
 maml_zoo_path = '/'.join(os.path.realpath(os.path.dirname(__file__)).split('/')[:-1])
 
@@ -27,15 +26,20 @@ TASKNAME = 'medium'
 
 def maml_test(experiment, config, sess, start_itr, pkl):
 
-    config['meta_batch_size'] = 20
-    config['rollouts_per_meta_task'] = 5
+    # config['meta_batch_size'] = 20
+    config['rollouts_per_meta_task'] = 10
     config['max_path_length'] = 150
 
-    from medium_env_list import TRAIN_DICT, TRAIN_ARGS_KWARGS
+    from metaworld.envs.mujoco.env_dict import MEDIUM_MODE_CLS_DICT, MEDIUM_MODE_ARGS_KWARGS
+    # goals are sampled and set anyways so we don't care about the default goal of reach
+    # pick_place, push are the same.
     env = MultiClassMultiTaskEnv(
-        task_env_cls_dict=TRAIN_DICT,
-        task_args_kwargs=TRAIN_ARGS_KWARGS)
-    
+        task_env_cls_dict=MEDIUM_MODE_CLS_DICT['train'],
+        task_args_kwargs=MEDIUM_MODE_ARGS_KWARGS['train'],
+        sample_goals=True,
+        obs_type='plain',
+    )
+
     baseline = LinearFeatureBaseline()
     policy = experiment['policy']
     # policy.meta_batch_size = N_TASKS
