1e170d66dc72df9958fadc019a7717be04e5549a
zhanpenghe <zhanpeng@cs.columbia.edu>
2019-09-08 19:20:06 -0400

    Propagate changes to hardmode maml evals

--- corl/hard_test_test_envs.py
+++ corl/hard_test_test_envs.py
@@ -86,6 +86,85 @@ def maml_test(experiment, config, sess, start_itr, pkl):
     trainer.train(test_time=True)
 
 
+def maml_test_batch(folder, config, start_itr):
+
+    config['rollouts_per_meta_task'] = 10
+    config['max_path_length'] = 150
+
+    from metaworld.envs.mujoco.env_dict import HARD_MODE_CLS_DICT, HARD_MODE_ARGS_KWARGS
+
+    # goals are sampled and set anyways so we don't care about the default goal of reach
+    # pick_place, push are the same.
+    env = MultiClassMultiTaskEnv(
+        task_env_cls_dict=HARD_MODE_CLS_DICT['test'],
+        task_args_kwargs=HARD_MODE_ARGS_KWARGS['test'],
+        sample_goals=True,
+        obs_type='plain',
+        sample_all=True,
+    )
+    config['meta_batch_size'] = len(HARD_MODE_CLS_DICT['train'].keys())
+
+    baseline = LinearFeatureBaseline()
+    sample_processor = MAMLSampleProcessor(
+        baseline=baseline,
+        discount=config['discount'],
+        gae_lambda=config['gae_lambda'],
+        normalize_adv=config['normalize_adv'],
+        positive_adv=config['positive_adv'],
+    )
+
+    sampler = MAMLSampler(
+        env=env,
+        policy=None,
+        rollouts_per_meta_task=config['rollouts_per_meta_task'],  # This batch_size is confusing
+        meta_batch_size=config['meta_batch_size'],
+        max_path_length=config['max_path_length'],
+        parallel=config['parallel'],
+        envs_per_task=config['envs_per_task'],
+    )
+
+    all_pkls = [f for f in listdir(folder) if '.pkl' in f]
+    for p in all_pkls:
+        full_path = os.path.join(folder, p)
+        eval_single(env, full_path, sampler, sample_processor, config)
+
+
+def eval_single(env, pkl_file_path, sampler, sample_processor, config):
+    """
+    load policy-> replace sampler's policy-> rebuild tf graph wit new session-> eval
+    """
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            with open(pkl_file_path, 'rb') as f:
+                experiment = joblib.load(f)
+                policy = experiment['policy']
+                sampler.policy = policy
+
+                algo = TRPOMAML(
+                    policy=policy,
+                    step_size=config['step_size'],
+                    inner_type=config['inner_type'],
+                    meta_batch_size=config['meta_batch_size'],
+                    num_inner_grad_steps=config['num_inner_grad_steps'],
+                    inner_lr=config['inner_lr']
+                )
+                trainer = Trainer(
+                    algo=algo,
+                    policy=policy,
+                    env=env,
+                    sampler=sampler,
+                    sample_processor=sample_processor,
+                    n_itr=config['n_itr'],
+                    num_inner_grad_steps=config['num_inner_grad_steps'],  # This is repeated in MAMLPPO, it's confusing
+                    sess=sess,
+                    start_itr=0,
+                    pkl=pkl,
+                    name='hard_testenvs',
+                )
+
+                trainer.train(test_time=True)
+
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Play a pickled policy.')
     parser.add_argument('variant_index', metavar='variant_index', type=int,
@@ -129,17 +208,10 @@ if __name__=="__main__":
             json.dump(config, open(maml_zoo_path + '/data/maml_test/test_{}_{}_{}/params.json'.format(TASKNAME, idx, rand_num), 'w'))
             maml_test(experiment, config, sess, itr, pkl)
     elif folder:
-        for p in pkls:
-            with tf.Graph().as_default():
-                with tf.Session() as sess:
-                    with open(os.path.join(folder, p), 'rb') as file:
-                        experiment = joblib.load(file)
-                    logger.configure(dir=maml_zoo_path + '/data/maml_test/test_{}_{}_{}'.format(TASKNAME, idx, rand_num), format_strs=['stdout', 'log', 'csv'],
-                             snapshot_mode='all',)
-                    config = json.load(open(config_file, 'r'))
-                    json.dump(config, open(maml_zoo_path + '/data/maml_test/test_{}_{}_{}/params.json'.format(TASKNAME, idx, rand_num), 'w'))
-                    maml_test(experiment, config, sess, itr, p)
-            import gc
-            gc.collect()
+        logger.configure(dir=maml_zoo_path + '/data/maml_test/test_{}_{}_{}'.format(TASKNAME, idx, rand_num), format_strs=['stdout', 'log', 'csv'],
+            snapshot_mode='all',)
+        config = json.load(open(config_file, 'r'))
+        json.dump(config, open(maml_zoo_path + '/data/maml_test/test_{}_{}_{}/params.json'.format(TASKNAME, idx, rand_num), 'w'))
+        maml_test_batch(folder, config, itr)
     else:
         print('Please provide a pkl file')
